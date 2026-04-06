7c7a13615c64d1b1a8b5497e3013179119b261c5
RMadhumitha30 <rmadhumitha9786@gmail.com>
2026-04-06 16:05:33 +0530

    new

---
+++ fuel.java
@@ -0,0 +1,15 @@
+public class fuel {
+    public static void main(String[] args) {
+        int distance=50;
+        int mileage=45;
+        int fuel=100;
+        int total_cost=(distance/mileage)*fuel;
+        System.out.println(total_cost);
+
+
+
+
+
+    }
+    
+}
